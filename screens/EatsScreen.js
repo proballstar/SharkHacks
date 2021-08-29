5eadcde1118b0bb8926c789a13c92a98433b2fe6
Ifo Ikede <ifoitech@gmail.com>
2021-08-29 02:55:57 -0300

    fix bug .. changed EatsScreen to use return( )  not render( )

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -10,7 +10,7 @@ import { render } from 'react-dom';
 
 const EatsScreen = () => {
 
-  render(
+  return (
     <view>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
