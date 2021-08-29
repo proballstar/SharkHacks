6aaee4ad91de08d89f5a2aff6a8b16d1dd85d22f
Ifo Ikede <ifoitech@gmail.com>
2021-08-29 02:06:27 -0300

    Creation of the eats branch ..

---
+++ screens/EatsScreen.js
@@ -0,0 +1,24 @@
+// EatScreen.js
+
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import Restaurant from '../Restaurants';
+
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env"
+import { KeyboardAvoidingView } from 'react-native';
+import { render } from 'react-dom';
+
+const EatsScreen = () => {
+
+  render(
+    <view>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Getting the best places in town</title>
+      <ol id="places" />
+    </view>
+  )
+}
+
+export default EatsScreen
